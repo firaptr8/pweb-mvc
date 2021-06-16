93b7834774766e554ea678bbec2ee1d10fb539bb
firaptr8 <71923133+firaptr8@users.noreply.github.com>
2021-06-16 19:13:29 +0700

    Create Home.php

---
+++ mvcphp/app/controllers/Home.php
@@ -0,0 +1,12 @@
+<?php
+
+class Home extends Controller{
+    public function index()
+	{
+		$data['title'] = 'Halaman Home';
+
+		$this->view('templates/header', $data);
+		$this->view('home/index', $data);
+		$this->view('templates/footer');
+	}
+}
