f16c2c71805b853f734946f80d2cfc2e9921179a
firaptr8 <71923133+firaptr8@users.noreply.github.com>
2021-06-16 19:18:55 +0700

    Create index.php

---
+++ mvcphp/app/views/blog/index.php
@@ -0,0 +1,123 @@
+<div class="fh5co-hero">
+	<div class="fh5co-overlay"></div>
+		<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(<?= base_url; ?>/images/cover2.jpg);">
+			<div class="desc animate-box">
+				<h2>Our <strong>Blog &amp; News</strong></h2>
+				<span>Giving Is The Greatest Act Of Grace.</span>
+				<span><a class="btn btn-primary btn-lg" href="<?= base_url; ?>/donate">Donate Now</a></span>
+			</div>
+		</div>
+
+</div>
+		
+		<div id="fh5co-blog-section" class="fh5co-section-gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+						<h3>Read. Learn. Share</h3>
+						<p>Berikut merupakan informasi donasi yang membutuhkan pertolongan segera.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="fh5co-blog animate-box">
+							<a href="#"><img class="img-responsive" src="<?= base_url; ?>/images/cover1.jpg" alt=""></a>
+							<div class="blog-text">
+								<div class="prod-title">
+									<h3><a href=""#>Bantu Anak-Anak di Daerah Sukamandang, Seruyan, Kalimantan Tengah Agar Bisa Memperoleh Pendidikan dengan Layak</a></h3>
+									<span class="posted_by">15 September</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Daerah Sukamandang, Kabupaten Seruyan, Provinsi Kalimantan Tengah adalah salah satu contoh daerah yang masih sangat kurang dalam memberikan pelayanan pendidikan pada anak-anak.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="fh5co-blog animate-box">
+							<a href="#"><img class="img-responsive" src="<?= base_url; ?>/images/cover2.jpg" alt=""></a>
+							<div class="blog-text">
+								<div class="prod-title">
+									<h3><a href=""#>Salurkan Donasi untuk Korban Bencana Banjir di NTT</a></h3>
+									<span class="posted_by">31 Mei</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Rumah-rumah warga terendam air banjir setelah banjir di Ile Ape, Pulau Lembata, provinsi Nusa Tenggara Timur, diterjang banjir bandang dan tanah longsor.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="fh5co-blog animate-box">
+							<a href="#"><img class="img-responsive" src="<?= base_url; ?>/images/cover3.jpg" alt=""></a>
+							<div class="blog-text">
+								<div class="prod-title">
+									<h3><a href=""#>Ayo Bantu Beri Konsumsi Warga yang Menjadi Korban Longsor di Tangsel</a></h3>
+									<span class="posted_by">20 Mei</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Pada minggu siang, belasan penghuni rumah yang terdampak longsor baru saja direlokasi ke tempat yang lebih aman.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+				</div>
+
+				<div class="container">
+				<div class="row row-bottom-padded-md">
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="fh5co-blog animate-box">
+							<a href="#"><img class="img-responsive" src="<?= base_url; ?>/images/cover4.jpg" alt=""></a>
+							<div class="blog-text">
+								<div class="prod-title">
+									<h3><a href=""#>Bantu Sekolahkan Anak-Anak di daerah Luwuk, Banggai, Sulawesi Tengah</a></h3>
+									<span class="posted_by">18 September</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Kecamatan Luwuk, Kabupaten Banggai, Provinsi Sulawesi Tengah adalah daerah terpencil yang masih tertinggal dalam masalah pendidikan anak.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="fh5co-blog animate-box">
+							<a href="#"><img class="img-responsive" src="<?= base_url; ?>/images/cover2.jpg" alt=""></a>
+							<div class="blog-text">
+								<div class="prod-title">
+									<h3><a href=""#>Salurkan Donasi untuk Korban Bencana Banjir di NTT</a></h3>
+									<span class="posted_by">31 Mei</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Rumah-rumah warga terendam air banjir setelah banjir di Ile Ape, Pulau Lembata, provinsi Nusa Tenggara Timur, diterjang banjir bandang dan tanah longsor.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+					<div class="clearfix visible-sm-block"></div>
+					<div class="col-lg-4 col-md-4 col-sm-6">
+						<div class="fh5co-blog animate-box">
+							<a href="#"><img class="img-responsive" src="<?= base_url; ?>/images/cover3.jpg" alt=""></a>
+							<div class="blog-text">
+								<div class="prod-title">
+									<h3><a href=""#>Ayo Bantu Beri Konsumsi Warga yang Menjadi Korban Longsor di Tangsel</a></h3>
+									<span class="posted_by">20 Mei</span>
+									<span class="comment"><a href="">21<i class="icon-bubble2"></i></a></span>
+									<p>Pada minggu siang, belasan penghuni rumah yang terdampak longsor baru saja direlokasi ke tempat yang lebih aman.</p>
+									<p><a href="#">Learn More...</a></p>
+								</div>
+							</div> 
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4 text-center animate-box">
+						<a href="#" class="btn btn-primary btn-lg">Our Blog</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<!-- fh5co-blog-section -->
