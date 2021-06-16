bdd6b7f04804c71911922f96d6b6da5dc395d60f
firaptr8 <71923133+firaptr8@users.noreply.github.com>
2021-06-16 19:23:24 +0700

    Create index.php

---
+++ mvcphp/app/views/info/index.php
@@ -0,0 +1,174 @@
+<div class="fh5co-hero">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(<?= base_url; ?>/images/cover2.jpg);">
+				<div class="desc animate-box">
+					<h2><strong>Our Donate Information</strong></h2>
+					<span> Sisihkan Rejeki Untuk Berbagi</span>
+					<span><a class="btn btn-primary btn-lg" href="<?= base_url; ?>/donate">Donate Now</a></span>
+				</div>
+			</div>
+
+		</div>
+		
+		<div id="fh5co-feature-product" class="fh5co-section-gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center heading-section">
+						<h3>Jalinan Tali Rezeki</h3>
+					
+					</div>
+                    <div class="container" style="margin-top: 20px">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h2 style="text-align: center;margin-bottom: 30px">Data Perolehan Donasi Saat Ini Dan Penyalurannya</h2>
+                                <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%" >
+                                <p>Perolehan Donasi</p>
+                            
+                                <thead>
+                                
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Pendonasi</th>
+                                        <th>Jumlah Donasi</th>
+                                        <th>Jenis Donasi</th>
+                                        <th>Total Seluruh Donasi</th>
+                                       
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Hamba Allah</td>
+                                                <td>200.000</td>
+                                                <td>donasi pendidikan</td>
+                                                <td>200.000</td>
+                                            </tr> 
+
+                                             <tr>
+                                                <td>2</td>
+                                                <td>Rahmat</td>
+                                                <td>100.000</td>
+                                                <td>donasi pendidikan</td>
+                                                <td>300.000</td>
+                                            </tr>  
+
+                                             <tr>
+                                                <td>3</td>
+                                                <td>Ftri</td>
+                                                <td>1.000.000</td>
+                                                <td>donasi bencana alam</td>
+                                                <td>1.300.000</td>
+                                            </tr>  
+
+                                             <tr>
+                                                <td>4</td>
+                                                <td>Mahmudi</td>
+                                                <td>2.000.000</td>
+                                                <td>donasi bencana alam</td>
+                                                <td>3.300.000</td>
+                                            </tr>
+
+                                             <tr>
+                                                <td>5</td>
+                                                <td>Raihan</td>
+                                                <td>200.000</td>
+                                                <td>donasi kebutuhan pokok</td>
+                                                <td>3.500.000</td>
+                                            </tr>  
+
+                                             <tr>
+                                                <td>6</td>
+                                                <td>Lala</td>
+                                                <td>1.000.000</td>
+                                                <td>donasi kebutuhan pokok</td>
+                                                <td>4.500.000</td>
+                                            </tr> 
+
+                                             <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td>TOTAL AKHIR</td>
+                                                <td>4.500.000</td>
+                                            </tr>   
+
+
+                                            <div class="col-md-13">     
+                                <h2 style="text-align: center;margin-bottom: 30px"></h2>
+                                <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%" >
+                                <p>Penyaluran Donasi</p>
+                                <thead>
+                                
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Penerima</th>
+                                        <th>Jumlah Donasi</th>
+                                        <th>Jenis Donasi</th>
+                                        
+                                    
+                                       
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    
+                                            <tr>
+                                                <td>1</td>
+                                                <td>Zuhro, siswa SD</td>
+                                                <td>250.000</td>
+                                                <td>donasi pendidikan</td>
+                                                
+                                            </tr> 
+
+                                             <tr>
+                                                <td>2</td>
+                                                <td>Fatih, siswa SD</td>
+                                                <td>250.000</td>
+                                                <td>donasi pendidikan</td>
+                                               
+                                            </tr>  
+
+                                             <tr>
+                                                <td>3</td>
+                                                <td>Masyarakat korban Gempa di Malang</td>
+                                                <td>1.500.000</td>
+                                                <td>donasi bencana alam</td>
+                                                
+                                            </tr>  
+
+                                             <tr>
+                                                <td>4</td>
+                                                <td>Masyarakat korban banjir bandang dan longsor NTT dan NTB</td>
+                                                <td>1.500.000</td>
+                                                <td>donasi bencana alam</td>
+                                                
+                                            </tr>
+
+                                             <tr>
+                                                <td>5</td>
+                                                <td>Munaroh , pegawai yang di phk akibat pandemi</td>
+                                                <td>600.000</td>
+                                                <td>donasi kebutuhan pokok</td>
+                                                
+                                            </tr>  
+
+                                             <tr>
+                                                <td>6</td>
+                                                <td>Mimin, pegawai yang di phk akibat pandemi</td>
+                                                <td>600.000</td>
+                                                <td>donasi kebutuhan pokok</td>
+                                               
+                                            </tr> 
+                                </tbody>
+                                </table>
+                            </div>
+                        </div>   
+                    </div>
+   
+                    </div>        
+				</div>
+			</div>
+		</div>
+		<!-- fh5co-content-section -->
+
+
