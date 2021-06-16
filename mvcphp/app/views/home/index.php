0f9942c7db1ac030feec90737a971530711a2693
firaptr8 <71923133+firaptr8@users.noreply.github.com>
2021-06-16 19:22:35 +0700

    Create index.php

---
+++ mvcphp/app/views/home/index.php
@@ -0,0 +1,210 @@
+<div class="fh5co-hero">
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(<?= base_url; ?>/images/cover2.jpg);">
+				<div class="desc animate-box">
+					<h2><strong>Donate</strong> for the <strong>Poor People</strong></h2>
+					<span>Charity Is About Love.</span> 
+					<span><a class="btn btn-primary btn-lg" href="<?= base_url; ?>/donate">Donate Now</a></span>
+				</div>
+			</div>
+		</div>
+		
+		<div id="fh5co-feature-product" class="fh5co-section-gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center heading-section">
+						<h3>Giving is Virtue.</h3>
+						<p>Memberi itu ibarat sungai mengalir, kita hanya terus memperoleh manfaat dari air bersihnya.</p>
+					</div>
+				</div>
+
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-12 text-center animate-box">
+						<p><img src="<?= base_url; ?>/images/cover1.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive"></p>
+					</div>
+					<div class="col-md-6 text-center animate-box">
+						<p><img src="<?= base_url; ?>/images/cover2.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive"></p>
+					</div>
+					<div class="col-md-6 text-center animate-box">
+						<p><img src="<?= base_url; ?>/images/cover3.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive"></p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="feature-text">
+							<h3>Menjadi Relawan</h3>
+							<p>Yayasan kami siap menampung anda sekalian yang ingin menjadi relawan sukarela. Memberi dan melihat langsung senyum bahagia dari mereka yang kita bantu.</p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="feature-text">
+							<h3>Memberi Dengan Bahagia</h3>
+							<p>Rasakan langsung energi positif yang mereka salurkan pada kita lewat kebahagiaan yang terpancar di wajah mereka.</p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="feature-text">
+							<h3>Donasi</h3>
+							<p>Setiap tindakan kebaikan adalah amal. Mari berdonasi dengan bahagia!</p>
+						</div>
+					</div>
+				</div>
+
+				
+			</div>
+		</div>
+
+		
+		<div id="fh5co-portfolio">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center heading-section animate-box">
+						<h3>Our Gallery</h3>
+						<p>Senyuman mereka adalah sesuatu yang membuat hati terasa damai. Do good and good will come to you.</p>
+					</div>
+				</div>
+
+				
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-12">
+						<ul id="fh5co-portfolio-list">
+
+							<li class="two-third animate-box" data-animate-effect="fadeIn" style="background-image: url(<?= base_url; ?>/images/cover1.jpg); ">
+								<a href="#" class="color-3">
+									<div class="case-studies-summary">
+										<span>Give Love</span>
+										<h2>Donation is caring</h2>
+									</div>
+								</a>
+							</li>
+						
+							<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(<?= base_url; ?>/images/cover2.jpg); ">
+								<a href="#" class="color-4">
+									<div class="case-studies-summary">
+										<span>Give Love</span>
+										<h2>Donation is caring</h2>
+									</div>
+								</a>
+							</li>
+
+							<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(<?= base_url; ?>/images/cover3.jpg); "> 
+								<a href="#" class="color-5">
+									<div class="case-studies-summary">
+										<span>Give Love</span>
+										<h2>Donation is caring</h2>
+									</div>
+								</a>
+							</li>
+							<li class="two-third animate-box" data-animate-effect="fadeIn" style="background-image: url(<?= base_url; ?>/images/cover4.jpg); ">
+								<a href="#" class="color-6">
+									<div class="case-studies-summary">
+										<span>Give Love</span>
+										<h2>Donation is caring</h2>
+									</div>
+								</a>
+							</li>
+						</ul>		
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4 col-md-offset-4 text-center animate-box">
+						<a href="#" class="btn btn-primary btn-lg">See Gallery</a>
+					</div>
+				</div>
+
+				
+			</div>
+		</div>
+		
+
+		
+		<div id="fh5co-content-section" class="fh5co-section-gray">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+						<h3>Our Philanthropist</h3>
+						<p>Beribu ucapan terima kasih masih tidak sebanding dengan apa yang sudah mereka berikan pada anak-anak di luaran sana yang sedang berjuang meraih mimpinya, serta orang-orang yang sedang membutuhkan bantuan terdesak.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="fh5co-testimonial text-center animate-box">
+							<figure>
+								<img src="<?= base_url; ?>/images/person1.jpg" alt="user">
+							</figure>
+							<blockquote>
+								<p>&ldquo;What actions are the most excellent? To gladden the heart of human beings, to feed the hungry, to help the afflicted, to lighten the sorrow of the sorrowful and to remove the sufferings of the injured.&rdquo;</p>
+							</blockquote>
+							<span>Johnson Sam, XYZ Co.</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="fh5co-testimonial text-center animate-box">
+							<figure>
+								<img src="<?= base_url; ?>/images/person2.jpg" alt="user">
+							</figure>
+							<blockquote>
+								<p>&ldquo;From a tiny sparks, may burst a mighty flame. Do whatever is the slightest good, for you will not know what kindness put you in.&rdquo;</p>
+							</blockquote>
+							<span>Jeanneth Doe, XYZ Co.</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="fh5co-testimonial text-center animate-box">
+							<figure>
+								<img src="<?= base_url; ?>/images/person3.jpg" alt="user">
+							</figure>
+							<blockquote>
+								<p>&ldquo;Kindness is a language which the deaf can hear and the blind can see. In necessary things, unity; in doubtful things, liberty; in all things, charity.&rdquo;</p>
+							</blockquote>
+							<span>Richard Baxter, XYZ Co.</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- fh5co-content-section -->
+
+		<div id="fh5co-services-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+						<h3>Our Projects. Support Us</h3>
+						<p>Beberapa program yang kami adakan untuk membantu anak-anak yang kurang beruntung dalam menggapai asa dan mewujudkan mimpi mereka. Serta program lainnya untuk orang-orang yang sedang membutuhkan pertolongan.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-md-4 col-sm-4">
+						<div class="services animate-box">
+							<span><i class="icon-heart"></i></span>
+							<h3>Beasiswa Untuk Anak</h3>
+							<p>Menyalurkan bantuan pada anak sekolah yang membutuhkan bantuan dalam biaya pendidikan.</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="services animate-box">
+							<span><i class="icon-heart"></i></span>
+							<h3>Bantuan Kebutuhan Pokok</h3>
+							<p>Menyalurkan bantuan sembilan bahan pokok untuk mereka yang sedang terdesak dalam memenuhi kebutuhan sehari-hari.</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="services animate-box">
+							<span><i class="icon-heart"></i></span>
+							<h3>Sumbangan Bencana Alam</h3>
+							<p>Menyalurkan bantuan donasi kepada saudara kita yang sedang dilanda musibah.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- END What we do -->
+
+
+		
