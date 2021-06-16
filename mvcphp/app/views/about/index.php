8bba2228145b286a58e75947ed0458772f808f21
firaptr8 <71923133+firaptr8@users.noreply.github.com>
2021-06-16 19:17:56 +0700

    Create index.php

---
+++ mvcphp/app/views/about/index.php
@@ -0,0 +1,48 @@
+<div class="fh5co-hero">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(<?= base_url; ?>/images/cover2.jpg);">
+				<div class="desc animate-box">
+					<h2><strong>We Are Non-Profit</strong></h2>
+					<span>Charity Is Kind</span>
+					<span><a class="btn btn-primary btn-lg" href="<?= base_url; ?>/donate">Donate Now</a></span>
+				</div>
+			</div>
+
+		</div>
+
+		<div id="fh5co-services-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+						<h3>JTR (Jalinan Tali Rezeki)</h3>
+						<p>JTR atau Jalinan Tali Rezeki merupakan website untuk mengumpulkan donasi dan menyalurkannya kepada yang membutuhkan. Donasi ini akan disalurkan pada 3 bidang yakni donasi pendidikan, donasi bencana alam dan donasi kebutuhan pokok. Donasi ini akan disalurkan lewat sebuah yayasan yang bernama Pelita Hati.</p>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row text-center">
+					<div class="col-md-4 col-sm-4">
+						<div class="services animate-box">
+							<span><i class="icon-heart"></i></span>
+							<h3>Donasi Pendidikan</h3>
+							<p>Donasi ini akan di berikan kepada siswa atau anak yang memiliki masalah dalam pembayaran pendidikannya. Dalam proses penyalurannya juga terdapat seleksi yang dilakukan oleh penyalur supaya donasi tersebut tepat sasaran.</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="services animate-box">
+							<span><i class="icon-heart"></i></span>
+							<h3>Donasi Bencana Alam</h3>
+							<p>Donasi ini ditujukan kepada masyarat yang sedang terkena dampak bencana alam, donasi ini akan di berikan dalam bentuk kebutuhan pokok, baju serta uang.</p>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-4">
+						<div class="services animate-box">
+							<span><i class="icon-heart"></i></span>
+							<h3>Donasi Kebutuhan Pokok</h3>
+							<p>Donasi ini ditujukan kepada masyarakat yang kurang mampu dalam kehidupannya , apalagi saat keadaan pandemi seperti sekarang ini. Banyak sekali orang kehilangan mata pencahariannya. Donasi ini akan disalurkan berupa kebutuhan pokok seperti beras, telur dan lain-lain.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
