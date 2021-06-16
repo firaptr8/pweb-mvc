68a013aa4fd36a2e80588d7e0dddb37207714d89
firaptr8 <71923133+firaptr8@users.noreply.github.com>
2021-06-16 19:19:56 +0700

    Create index.php

---
+++ mvcphp/app/views/contact/index.php
@@ -0,0 +1,57 @@
+<div class="fh5co-hero">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover text-center" data-stellar-background-ratio="0.5" style="background-image: url(<?= base_url; ?>/images/cover1.jpg);">
+				<div class="desc animate-box">
+					<h2><strong>Contact</strong> Us</h2>
+					<span>We Rise By Lifting Others.</span>
+					<span><a class="btn btn-primary btn-lg" href="<?= base_url; ?>/donate">Donate Now</a></span>
+				</div>
+			</div>
+
+		</div>
+		
+		<div id="fh5co-contact" class="animate-box">
+			<div class="container">
+				<form action="#">
+					<div class="row">
+						<div class="col-md-6">
+							<h3 class="section-title">Our Address</h3>
+							<p>Kabupaten Jember, Jawa Timur.</p>
+							<ul class="contact-info">
+								<li><i class="icon-location-pin"></i>Jl.Gajah Mada No.198 Kaliwates, Jember</li>
+								<li><i class="icon-phone2"></i>+6287-934-560-021</li>
+								<li><i class="icon-mail"></i><a href="#">info@jalinantalirezeki.com</a></li>
+								<li><i class="icon-globe2"></i><a href="#">www.jalinantalirezeki.com</a></li>
+							</ul>
+						</div>
+						<div class="col-md-6">
+							<div class="row">
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-12">
+									<div class="form-group">
+										<input type="submit" value="Send Message" class="btn btn-primary">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		<!-- END fh5co-contact -->
+		<div id="map" class="fh5co-map"></div>
+		<!-- END map -->
